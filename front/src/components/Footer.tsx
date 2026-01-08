b8dfd06706806e077f90f0f3ce7266ba81e8d21e
TRAORE LACINA <traorelac01@gmail.com>
2026-01-08 19:55:46 +0000

    mis a jour

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -2,6 +2,16 @@
 
 import { Facebook, Instagram, Twitter, Mail, Phone, MapPin } from 'lucide-react';
 
+const TikTokIcon = () => (
+  <svg 
+    className="w-5 h-5" 
+    fill="currentColor" 
+    viewBox="0 0 24 24"
+  >
+    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+  </svg>
+);
+
 interface FooterProps {
   onNavigate?: (page: string) => void;
 }
@@ -26,9 +36,9 @@ export default function Footer({ onNavigate }: FooterProps) {
   ];
 
   const socialLinks = [
-    { icon: Facebook, url: '#', name: 'Facebook' },
-    { icon: Instagram, url: '#', name: 'Instagram' },
-    { icon: Twitter, url: '#', name: 'Twitter' },
+    { icon: Facebook, url: 'https://web.facebook.com/AffoueRose107', name: 'Facebook' },
+   
+    { icon: TikTokIcon, url: 'https://www.tiktok.com/@kouakourose3?_r=1&_t=ZP-913v3HNoSB5', name: 'TikTok' },
   ];
 
   return (
